b7e6c1a875ce4787ed3fde2b601397ef9605b8e0
websturing <websturing.project@gmail.com>
2020-04-18 16:24:24 +0700

    change URL to Online

--- resources/js/url.js
+++ resources/js/url.js
@@ -2,9 +2,9 @@
 // const urlBase = "disperindag_sembako/panelAdmin";
 // const urlWeb = "http://localhost/disperindag_sembako";
 
-const urlAxios = "http://tukangketik.my.id/disperindag_sembako/api";
-const urlBase = "disperindag_sembako/panelAdmin";
-const urlWeb = "http://tukangketik.my.id/disperindag_sembako";
+const urlAxios = "http://tukangketik.my.id/disperindag/api";
+const urlBase = "disperindag/panelAdmin";
+const urlWeb = "http://tukangketik.my.id/disperindag";
 
 export default {
     urlAxios: urlAxios,
